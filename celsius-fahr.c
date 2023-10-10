a60e565bcb31de8a09338e50eb39aff4487e3f09
Delon <146134366+DazaiX88@users.noreply.github.com>
2023-10-10 17:35:46 -0400

    Add files via upload
    This file does the opposite of the temp files and converts celsius to Fahrenheit using floating point for more accuracy

---
+++ celsius-fahr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/* this file creates an accurate temperature using floating point*/
+/* print celsius to fahrenheit table
+    for fahr = 0, 10, ..., 300; floating-point version */
+/* celsius to fahr exercise*/
+
+main()
+{
+    float celsius, fahr;
+    int lower, upper, step;
+    
+    lower = -17; /* lower limit of temperature table */
+    upper = 300; /* upper limitn*/
+    step = 10; /*step size*/
+    
+    celsius = lower;
+    while (celsius <= upper) {
+        fahr = 32.0 + (celsius * 1.8);        printf("%5.1f %6.1f\n", celsius, fahr);
+        celsius = celsius + step;
+    }
+}
